eb21c6241a8cf0c381c3d8c7b40db8f4b92a31e1
Robert Seifert <rseifert.phone@gmail.com>
2015-09-03 19:21:38 -0400

    Fixed overworld not being loaded, no clue why FML insists it has to be loaded

--- src/main/java/com/builtbroken/mc/testing/junit/world/FakeWorldServer.java
+++ src/main/java/com/builtbroken/mc/testing/junit/world/FakeWorldServer.java
@@ -2,6 +2,7 @@ package com.builtbroken.mc.testing.junit.world;
 
 import com.builtbroken.mc.testing.junit.server.FakeDedicatedServer;
 import net.minecraft.profiler.Profiler;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.WorldSettings;
 import net.minecraft.world.WorldType;
@@ -16,26 +17,39 @@ import java.io.File;
  */
 public class FakeWorldServer extends WorldServer
 {
-    static File baseFolder = new File(new File("."), "out/test/FakeWorldWithServer/");
+    public static File baseFolder = new File(new File("."), "out/test/FakeWorldWithServer/");
     File rootFolder;
 
-    public FakeWorldServer(String name, FakeWorldSaveHandler handler, int dimID, WorldSettings settings)
+    public FakeWorldServer(String name, MinecraftServer server, File file, FakeWorldSaveHandler handler, int dimID, WorldSettings settings)
     {
-        super(new FakeDedicatedServer(new File(baseFolder, name)), handler, name, dimID, settings, new Profiler());
-        rootFolder = new File(baseFolder, name);
+        super(server, handler, name, dimID, settings, new Profiler());
+        rootFolder = file;
     }
 
     public static FakeWorldServer newWorld(String name)
     {
+        return newWorld(new FakeDedicatedServer(new File(baseFolder, name)), name);
+    }
+
+    public static FakeWorldServer newWorld(MinecraftServer server, String name)
+    {
         WorldSettings settings = new WorldSettings(0, WorldSettings.GameType.SURVIVAL, false, false, WorldType.FLAT);
         WorldInfo worldInfo = new WorldInfo(settings, name);
         FakeWorldSaveHandler handler = new FakeWorldSaveHandler(worldInfo);
-        if (!DimensionManager.isDimensionRegistered(100))
+        if (DimensionManager.getWorld(0) == null)
         {
-            DimensionManager.registerProviderType(100, FakeWorldProvider.class, false);
-            DimensionManager.registerDimension(100, 100);
+            WorldSettings settings2 = new WorldSettings(0, WorldSettings.GameType.SURVIVAL, false, false, WorldType.FLAT);
+            WorldInfo worldInfo2 = new WorldInfo(settings, name);
+            FakeWorldSaveHandler handler2 = new FakeWorldSaveHandler(worldInfo2);
+
+            DimensionManager.setWorld(0, new FakeWorldServer("overworld", server, new File(baseFolder, name), handler2, 0, settings2));
         }
-        return new FakeWorldServer(name, handler, 100, settings);
+        if (!DimensionManager.isDimensionRegistered(10))
+        {
+            DimensionManager.registerProviderType(10, FakeWorldProvider.class, false);
+            DimensionManager.registerDimension(10, 10);
+        }
+        return new FakeWorldServer(name, server, new File(baseFolder, name), handler, 10, settings);
     }
 
     @Override
