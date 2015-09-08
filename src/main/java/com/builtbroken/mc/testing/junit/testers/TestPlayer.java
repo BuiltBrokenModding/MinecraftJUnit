8f7f8e2dd4dc1a7f4aa04d3d01a5babeec04b739
Robert Seifert <rseifert.phone@gmail.com>
2015-09-08 01:28:34 -0400

    Removed some unused methods in TestPlayer

--- src/main/java/com/builtbroken/mc/testing/junit/testers/TestPlayer.java
+++ src/main/java/com/builtbroken/mc/testing/junit/testers/TestPlayer.java
@@ -1,15 +1,11 @@
 package com.builtbroken.mc.testing.junit.testers;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.play.client.C15PacketClientSettings;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ItemInWorldManager;
 import net.minecraft.stats.StatBase;
-import net.minecraft.util.ChunkCoordinates;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
@@ -25,12 +21,6 @@ public class TestPlayer extends EntityPlayerMP
     public boolean canCommandSenderUseCommand(int i, String s) { return false; }
 
     @Override
-    public ChunkCoordinates getPlayerCoordinates()
-    {
-        return new ChunkCoordinates(0, 0, 0);
-    }
-
-    @Override
     public void addChatComponentMessage(IChatComponent chatmessagecomponent) {}
 
     @Override
@@ -38,24 +28,6 @@ public class TestPlayer extends EntityPlayerMP
 
     @Override
     public void openGui(Object mod, int modGuiId, World world, int x, int y, int z) {}
-
-    @Override
-    public boolean isEntityInvulnerable() { return true; }
-
-    @Override
-    public boolean canAttackPlayer(EntityPlayer player) { return false; }
-
-    @Override
-    public void onDeath(DamageSource source) { return; }
-
-    @Override
-    public void onUpdate() { return; }
-
-    @Override
-    public void travelToDimension(int dim) { return; }
-
-    @Override
-    public void func_147100_a(C15PacketClientSettings pkt) { return; }
 
     public void reset()
     {
