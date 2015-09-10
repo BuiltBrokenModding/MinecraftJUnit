2f9636d4b28667f6acd0ce2a8f935eba16a3971d
Robert Seifert <rseifert.phone@gmail.com>
2015-09-10 14:45:55 -0400

    Not sure why this was extending Test

--- src/main/java/com/builtbroken/mc/testing/junit/AbstractTest.java
+++ src/main/java/com/builtbroken/mc/testing/junit/AbstractTest.java
@@ -1,14 +1,11 @@
 package com.builtbroken.mc.testing.junit;
 
 import junit.framework.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestResult;
 
 /**
  * Created by robert on 1/6/2015.
  */
-public class AbstractTest extends Assert implements Test
+public class AbstractTest extends Assert
 {
     /**
      * Builds anything that all tests for this class need
@@ -26,18 +23,6 @@ public class AbstractTest extends Assert implements Test
     }
 
     public void tearDownForTest(String name)
-    {
-
-    }
-
-    @Override
-    public int countTestCases()
-    {
-        return 1;
-    }
-
-    @Override
-    public void run(TestResult result)
     {
 
     }
