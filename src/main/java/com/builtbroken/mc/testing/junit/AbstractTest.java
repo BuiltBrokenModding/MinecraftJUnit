0b4313bc31cff59ec9303fa66749edeca5bb89ec
Robert Seifert <rseifert.phone@gmail.com>
2015-10-19 23:38:29 -0400

    Changed AbstractTest to extend TestCase

--- src/main/java/com/builtbroken/mc/testing/junit/AbstractTest.java
+++ src/main/java/com/builtbroken/mc/testing/junit/AbstractTest.java
@@ -1,21 +1,23 @@
 package com.builtbroken.mc.testing.junit;
 
-import junit.framework.Assert;
+import junit.framework.TestCase;
 
 /**
+ * Prefab for running tests using the VoltzTestRunner. Must be implemented in order to
+ * run the tests as these methods will be called.
  * Created by robert on 1/6/2015.
  */
-public class AbstractTest extends Assert
+public class AbstractTest extends TestCase
 {
     /**
      * Builds anything that all tests for this class need
      */
-    public void setUpForEntireClass(){}
+    public void setUpForEntireClass() {}
 
     /**
      * Cleans up anything that was created for this test
      */
-    public void tearDownForEntireClass(){}
+    public void tearDownForEntireClass() {}
 
     public void setUpForTest(String name)
     {
